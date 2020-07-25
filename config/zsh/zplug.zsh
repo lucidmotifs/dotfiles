1a0b9c29fe21ef3a030d915d9aeae4337e006b25
Pablo Olmos de Aguilera Corradini <pablo@odac.co>
2020-07-25 04:47:30 +0200

    --wip-- [skip ci]

--- config/zsh/zplug.zsh
+++ config/zsh/zplug.zsh
@@ -1,7 +1,7 @@
 zplug 'zplug/zplug', hook-build:'zplug --self-manage'
-zplug 'modules/editor', from:prezto
+# zplug 'modules/editor', from:prezto
 zplug "$ZDOTDIR", from:local, use:"*.zsh"
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
 zplug "zsh-users/zsh-history-substring-search", defer:3
 zplug "mafredri/zsh-async"
-zplug "sindresorhus/pure", use:pure.zsh, as:theme
+# zplug "sindresorhus/pure", use:pure.zsh, as:theme
